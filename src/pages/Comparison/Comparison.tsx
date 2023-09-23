73640b8500e779d9afb32329897fb7e2bd33c1f1
avivsbt <avivsbt@gmail.com>
2023-09-23 21:30:20 +0300

    Comparison - title

--- src/pages/Comparison/Comparison.tsx
+++ src/pages/Comparison/Comparison.tsx
@@ -2,10 +2,12 @@ import React, { memo } from "react";
 import cn from "classnames";
 import styles from "./Comparison.module.scss";
 import CardsComparison from "features/Comparison/Cards/CardsComparison";
+import { Typography } from "antd";
 
 const Comparison: React.FC = () => {
   return (
     <section className={cn(styles.wrapper)}>
+      <Typography.Title className={cn(styles.title)}>Comparing the abilities of your SuperHero</Typography.Title>
       <CardsComparison />
     </section>
   );
